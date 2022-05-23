285e9fccb0ac5f383d6b48ff7b9d075b0178bb78
Dawid Płocki <dawid.plocki@gmail.com>
2022-05-23 19:27:16 +0200

    Add first migration

---
+++ Migrations/20220523172543_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShorteningWebService.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LinkMaps",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OriginalLink = table.Column<string>(type: "TEXT", nullable: false),
+                    Shorted = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LinkMaps", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LinkMaps");
+        }
+    }
+}
