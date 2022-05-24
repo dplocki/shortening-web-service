0737910a692b2367070203118d68b20bd03eb800
Dawid Płocki <dawid.plocki@gmail.com>
2022-05-24 16:24:12 +0200

    Add LinkMap controller

---
+++ Controllers/LinkMapController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShorteningWebService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LinkMapController : ControllerBase
+    {
+        private readonly DatabaseContext databaseContext;
+
+        public LinkMapController(DatabaseContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<LinkMap> Get()
+        {
+            return databaseContext.LinkMaps.ToArray();
+        }
+    }
+}
