3d790cbf564c6a71a4a9d213e4be8ebf0fd3c6cc
Dawid Płocki <dawid.plocki@gmail.com>
2022-05-26 20:42:09 +0200

    Remove warnings from DatabaseContext

--- DatabaseContext.cs
+++ DatabaseContext.cs
@@ -5,9 +5,9 @@ namespace ShorteningWebService
 {
     public class DatabaseContext : DbContext
     {
-        public DbSet<LinkMap> LinkMaps { get; set; }
-        public DbSet<LinkMapUse> LinkMapUses { get; set; }
-        public DbSet<LinkMapError> LinkMapErrors { get; set; }
+        public DbSet<LinkMap> LinkMaps { get; set; } = null!;
+        public DbSet<LinkMapUse> LinkMapUses { get; set; } = null!;
+        public DbSet<LinkMapError> LinkMapErrors { get; set; } = null!;
 
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {
